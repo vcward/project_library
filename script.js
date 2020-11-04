f8075cd3dbf2e7778ddd84894e525f10e1e936c6
Victoria Ward <ward.vickyc@gmail.com>
2020-11-03 20:20:56 -1000

    refactor to use classes

--- script.js
+++ script.js
@@ -1,52 +1,58 @@
-let myLibrary = [{ title: 'The Prestige', author: 'Christopher Priest', pages: 404, read: true }];
-
-function Book(title, author, pages, read) {
-    // constructor
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
-function addBookToLibrary(book) {
-    // do stuff here
-    myLibrary.push(book);
-}
+class Library {
+    constructor(bookLibrary) {
+        this.bookLibrary = bookLibrary;
+    }
 
-function displayBooks() {
-    document.getElementById('table-data').innerHTML = myLibrary.map((book, index) => {
-        return `<tr>
-            <td>${book.title}</td>
-            <td>${book.author}</td>
-            <td>${book.pages}</td>
-            <td><button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" onclick="toggleReadStatus(${index})">
-            ${book.read ? 'Read' : 'Not Read'}
-          </button></td>
-          <td><button type="button" class="btn btn-danger" data-toggle="button" aria-pressed="false" onclick="removeBook(${index})">
-            Delete
-          </button></td>
-        </tr>`
-    }).join('');
+    addBookToLibrary(book) {
+        this.bookLibrary.push(book);
+    }
+
+    displayBooks() {
+        document.getElementById('table-data').innerHTML = this.bookLibrary.map((book, index) => {
+            return `<tr>
+                <td>${book.title}</td>
+                <td>${book.author}</td>
+                <td>${book.pages}</td>
+                <td><button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" onclick="toggleReadStatus(${index})">
+                ${book.read ? 'Read' : 'Not Read'}
+              </button></td>
+              <td><button type="button" class="btn btn-danger" data-toggle="button" aria-pressed="false" onclick="removeBook(${index})">
+                Delete
+              </button></td>
+            </tr>`
+        }).join('');
+    }
 }
 
 function toggleReadStatus(index) {
-    myLibrary[index].read = !myLibrary[index].read;
-    displayBooks();
+    myLibrary.bookLibrary[index].read = !myLibrary.bookLibrary[index].read;
+    myLibrary.displayBooks();
 }
 
 function removeBook(index) {
-    myLibrary.splice(index, 1);
-    displayBooks();
+    myLibrary.bookLibrary.splice(index, 1);
+    myLibrary.displayBooks();
 }
 
-function submitNewBook(event) {
-    event.preventDefault();
-    const title = event.target.elements.bookTitle.value;
-    const author = event.target.elements.bookAuthor.value;
-    const pages = event.target.elements.bookTitle.value;
-    const read = event.target.elements.readBook.checked;
-    addBookToLibrary(new Book(title, author, pages, read));
-    displayBooks();
-}
-
-displayBooks();
+const testBook = new Book('The Prestige', 'Christopher Priest', 404, true);
+const myLibrary = new Library([]);
+myLibrary.addBookToLibrary(testBook);
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const title = e.target.elements.bookTitle.value;
+    const author = e.target.elements.bookAuthor.value;
+    const pages = e.target.elements.bookTitle.value;
+    const read = e.target.elements.readBook.checked;
+    myLibrary.addBookToLibrary(new Book(title, author, pages, read));
+    myLibrary.displayBooks();
+});
+myLibrary.displayBooks();
